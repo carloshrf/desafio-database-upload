928a26e049b2b6ae8c0f7186ed4368120a61ae1e
carloshrf <carlos_6282010@hotmail.com>
2020-04-21 23:27:19 -0300

    Initial Commit

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -1,18 +1,37 @@
 import { Router } from 'express';
 
 // import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
 // import DeleteTransactionService from '../services/DeleteTransactionService';
 // import ImportTransactionsService from '../services/ImportTransactionsService';
+import { getRepository } from 'typeorm';
+import CreateTransactionService from '../services/CreateTransactionService';
+import Transaction from '../models/Transaction';
 
 const transactionsRouter = Router();
 
 transactionsRouter.get('/', async (request, response) => {
-  // TODO
+  const transactionsRepository = getRepository(Transaction);
+  const repository = await transactionsRepository.find();
+
+  return response.json({ transactions: repository });
 });
 
 transactionsRouter.post('/', async (request, response) => {
-  // TODO
+  const { title, value, type, category_id } = request.body;
+
+  const createTransaction = new CreateTransactionService();
+
+  const transaction = await createTransaction.execute({
+    title,
+    value,
+    type,
+    category_id,
+  });
+
+  delete transaction.created_at;
+  delete transaction.updated_at;
+
+  return response.json(transaction);
 });
 
 transactionsRouter.delete('/:id', async (request, response) => {
